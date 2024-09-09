5293f1e244a24df4f005b755fd40661f653b70df
Taha <taha100_100@yahoo.com>
2024-09-09 23:04:02 +0300

    add user management functionalities

---
+++ Bulky_Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,131 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
+        public UserController(UserManager<IdentityUser> userManager, IUnitOfWork unitOfWork, RoleManager<IdentityRole> roleManager) {
+            _unitOfWork = unitOfWork;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+        public IActionResult RoleManagment(string userId) {
+
+            UserVM RoleVM = new UserVM() {
+                ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId, includeProperties:"Company"),
+                Roles = _roleManager.Roles.Select(i => new SelectListItem {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                Companies = _unitOfWork.Company.GetAll().Select(i => new SelectListItem {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+
+            RoleVM.ApplicationUser.Role = _userManager.GetRolesAsync(_unitOfWork.ApplicationUser.Get(u=>u.Id==userId))
+                    .GetAwaiter().GetResult().FirstOrDefault();
+            return View(RoleVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagment(UserVM userVM) {
+
+            string oldRole  = _userManager.GetRolesAsync(_unitOfWork.ApplicationUser.Get(u => u.Id == userVM.ApplicationUser.Id))
+                    .GetAwaiter().GetResult().FirstOrDefault();
+
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userVM.ApplicationUser.Id);
+
+
+            if (!(userVM.ApplicationUser.Role == oldRole)) {
+
+                if (userVM.ApplicationUser.Role == SD.Role_Company) {
+                    applicationUser.CompanyId = userVM.ApplicationUser.CompanyId;
+                }
+                if (oldRole == SD.Role_Company) {
+                    applicationUser.CompanyId = null;
+                }
+                _unitOfWork.ApplicationUser.Update(applicationUser);
+                _unitOfWork.Save();
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, userVM.ApplicationUser.Role).GetAwaiter().GetResult();
+
+            }
+            else {
+                if(oldRole==SD.Role_Company && applicationUser.CompanyId != userVM.ApplicationUser.CompanyId) {
+                    applicationUser.CompanyId = userVM.ApplicationUser.CompanyId;
+                    _unitOfWork.ApplicationUser.Update(applicationUser);
+                    _unitOfWork.Save();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _unitOfWork.ApplicationUser.GetAll(includeProperties: "Company").ToList();
+
+            foreach(var user in objUserList) {
+
+                user.Role=  _userManager.GetRolesAsync(user).GetAwaiter().GetResult().FirstOrDefault();
+
+                if (user.Company == null) {
+                    user.Company = new Company() {
+                        Name = ""
+                    };
+                }
+            }
+
+            return Json(new { data = objUserList });
+        }
+
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
+        {
+
+            var objFromDb = _unitOfWork.ApplicationUser.Get(u => u.Id == id);
+            if (objFromDb == null) 
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if(objFromDb.LockoutEnd!=null && objFromDb.LockoutEnd > DateTime.Now) {
+
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _unitOfWork.ApplicationUser.Update(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Operation Successful" });
+        }
+
+        #endregion
+    }
+}
